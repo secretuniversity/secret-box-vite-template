0536a8756b071042f5d378c78aec1c52c8cfac29
Laura Weindorf <laura@secretchaingirl.io>
2022-09-27 13:31:05 -0400

    initial

---
+++ app/src/components/SecretBox.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+h1 {
+	font-size: 150px;
+}
+</style>
